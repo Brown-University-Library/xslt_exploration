22e7233163922a95f74f5aaefb05e21f83389763
Birkin James Diana <birkin.diana@gmail.com>
2023-10-26 19:31:23 -0400

    adds comments and gitignore.

--- transform_code.py
+++ transform_code.py
@@ -1,7 +1,24 @@
 """
-Little example that shows an example of applying a stylesheet to an XML file.
-Note that the files are read as bytes, cuz otherwise the unicode-str will conflict...
-...with the encoding in the "XML declaration" (first line) of the xml files.
+Little example python-script that shows:
+- how to apply a stylesheet to an XML file
+- how a stylesheet can do more than just alter xml -- in this case, it performs a calculation
+
+Notes...
+- Note that the files are read as bytes, cuz otherwise the unicode-str will conflict
+  with the encoding in the "XML declaration" (first line) of the xml files.
+- If running under windows, and the `assert ( result_str == expected )` fails,
+  comment out the line -- it could be that windows is defaulting to a different newline character(s).
+
+Usage...
+  Assumptions:
+  - virtual-environment has been set up at sibling-level to xslt_exploration
+  - lxml has been installed (see requirements.txt) into it
+  Commands:
+  $ cd /to/xslt_exploration
+  $ source ../env/bin/activate
+  $ python ./transform_code.py
+
+  The 'print' statement at the end will print the result to the console.
 """
 
 from lxml import etree
